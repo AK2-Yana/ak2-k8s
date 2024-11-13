f61e9bb583d7ff086d910f0f6bcfb3f97e8057c4
“akki-to” <yanach1n919@gmail.com>
2024-11-13 21:40:21 +0900

    add redis

--- base/delete.sh
+++ base/delete.sh
@@ -1,11 +1,21 @@
-kubectl delete -f ./config/postgres-conf.yml
-kubectl delete -f ./config/posgres-secret.yml
-
 kubectl delete -f ./volumes/sc-lv.yml
 kubectl delete -f ./volumes/pv.yml
 kubectl delete -f ./volumes/pvc.yml
 
 kubectl delete -f ./service/service-patch.yml
 
-kubectl delete -f ./deployments/db-deployment-patch.yml
+# web
 kubectl delete -f ./deployments/web-deployment-patch.yml
+
+# DB
+kubectl delete -f ./config/postgres-conf.yml
+kubectl delete -f ./config/posgres-secret.yml
+kubectl delete -f ./deployments/db-deployment-patch.yml
+
+# redis 
+kubectl delete -f ./config/redis-config-map.yml
+kubectl delete -f ./service/redis-svc-patch.yml
+kubectl delete -f ./deployments/redis-sts.yml
+
+# Network
+kubectl delete -f ./network/nw.yml
--- base/deployment.sh
+++ base/deployment.sh
@@ -11,7 +11,9 @@ kubectl apply -f ./deployments/db-deployment-patch.yml
 kubectl apply -f ./deployments/web-deployment-patch.yml
 
 # redis 
-kubectl apply -f ./namespace/redis-ns.yml
-kubectl apply -f ./config/redis-config.yml
+kubectl apply -f ./config/redis-config-map.yml
 kubectl apply -f ./service/redis-svc-patch.yml
 kubectl apply -f ./deployments/redis-sts.yml
+
+# Network
+kubectl apply -f ./network/nw.yml
